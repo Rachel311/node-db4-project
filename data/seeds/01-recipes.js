698021665ac01323fe736d64f4b5a32c42870902
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-23 22:57:42 -0600

    wip

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,18 +1,10 @@
 exports.seed = function (knex) {
   return knex('recipes')
-    .del()
     .then(function () {
       return knex('recipes')
         .insert([
           {
-            recipe_id: 1,
             recipe_name: 'Snickerdoodles',
-            create_at: knex.fn.now()
-          },
-          {
-            recipe_id: 2,
-            recipe_name: 'Chocolate Chip',
-            create_at: knex.fn.now()
           }
         ])
     })
--- data/seeds/03-steps.js
+++ data/seeds/03-steps.js
@@ -1,7 +1,6 @@
 
 exports.seed = function(knex) {
   return knex('steps')
-    .del()
     .then(function () {
       return knex('steps')
         .insert([
